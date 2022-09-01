fc91777edf20a5c71f9597d2bbdfaf47988be485
LUPIN-JPN <78338403+LUPIN-JPN@users.noreply.github.com>
2022-09-01 21:11:18 +0900

    Delete algorithm directory

--- algorithm/Day2-3.js
+++
@@ -1,5 +0,0 @@
-const student = { name: "철수", age: 8 };
-const school = { name: "다람쥐 초등학교", teacher: "다람이" };
-
-student.school = school;
-console.log(student);
--- algorithm/Day2.js
+++
@@ -1,9 +0,0 @@
-const fruits = [];
-
-fruits.push("사과");
-fruits.push("바나나");
-fruits.push("파인애플");
-
-fruits[0] = "사과";
-fruits[1] = "바나나";
-fruits[2] = "파인애플";
